d9db1d4bb6bf7acfe192d400b227f2848d6e3563
Chris Clark <clark800@users.noreply.github.com>
2023-09-29 00:51:17 -0700

    Use single quotes

--- config/plugins/meta_tags.sh
+++ config/plugins/meta_tags.sh
@@ -2,6 +2,6 @@
 meta_tags() {
     if [ -e "$SOURCE_PATH" ]; then
         grep '^% ' "$SOURCE_PATH" |
-            sed "s|^% \([^:]*\): \(.*\)$|<meta name=\"\1\" content=\"\2\">|"
+            sed 's|^% \([^:]*\): \(.*\)$|<meta name="\1" content="\2">|'
     fi
 }
