05b70ac27dc667090e0a35fefc7fd9416548d193
Chris Clark <clark800@users.noreply.github.com>
2023-11-14 10:40:13 -0800

    Titlemap plugin

--- config/hooks/pre_hook.sh
+++ config/hooks/pre_hook.sh
@@ -1,15 +1,4 @@
 
 pre_hook() {
-    _titlemap() {
-        slug="$(slugify "$TITLE")"
-        slug_path="$CONFIG/titlemap/$slug"
-        if [ -e "$slug_path" ]; then
-            println "Warning: duplicate title slug $slug"
-        fi
-        println "$DEST_PATH" > "$slug_path"
-    }
-    rm -rf "${CONFIG?:}/titlemap"
-    TITLEMAP="$CONFIG/titlemap"
-    mkdir "$TITLEMAP"
-    process _titlemap "."
+    titlemap
 }
---
+++ plugins/titlemap.sh
@@ -0,0 +1,14 @@
+readonly TITLEMAP="$TMP/titlemap"
+
+titlemap() {
+    _titlemap() {
+        slug="$(slugify "$TITLE")"
+        slug_path="$TITLEMAP/$slug"
+        if [ -e "$slug_path" ]; then
+            println "WARNING: duplicate title slug $slug" >&2
+        fi
+        println "$DEST_PATH" > "$slug_path"
+    }
+    mkdir -p "$TITLEMAP"
+    process _titlemap "."
+}
