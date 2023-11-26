5289b4644e70ea956202834685b60407c7b1c5ef
Chris Clark <clark800@users.noreply.github.com>
2023-11-26 14:08:52 -0800

    Only check first block of text for metadata

--- plugins/meta_tags.sh
+++ plugins/meta_tags.sh
@@ -1,7 +1,9 @@
 
 meta_tags() {
     if [ -e "$SOURCE_PATH" ]; then
-        sed -n 's|^= \([^:]*\): \(.*\)$|<meta name="\1" content="\2">|p' \
-            "$SOURCE_PATH"
+        A='/[^[:space:]]/,/^[[:space:]]*$/'
+        S='s|^= \([^:]*\): \(.*\)$|<meta name="\1" content="\2">|p'
+        Q='/^[[:space:]]*$/q'
+        sed -n "$A{$S;$Q;}" "$SOURCE_PATH"
     fi
 }
