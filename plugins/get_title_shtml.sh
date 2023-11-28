4d5f0b31cdd776e59ec2a8931a277e96fb10eab9
Chris Clark <clark800@users.noreply.github.com>
2023-11-27 22:48:21 -0800

    Improve get_title_shtml

--- plugins/get_title_shtml.sh
+++ plugins/get_title_shtml.sh
@@ -1,10 +1,10 @@
 
 get_title_shtml() {
     _extract() {
-        cat "$SOURCE_PATH" | tr -d '\r\n' |
-            sed -n -e "s|</\\($1\\)>.*|</\\1>|" \
-                   -e "s|.*<$1>\\(.*\\)</$1>.*|\\1|p" \
-                   -e 's|^[[:space:]]*||' -e 's|[[:space:]]*$||'
+        cat "$SOURCE_PATH" |
+            sed -n "/<$1>/,\${p;/<\/$1>/q;}" | tr -d '\r\n' |
+            sed -n -e "s|.*<$1>\\(.*\\)</$1>.*|\\1|" \
+                   -e 's|^[[:space:]]*||' -e 's|[[:space:]]*$||' -e 'p'
     }
 
     for tag in '[Tt][Ii][Tt][Ll][Ee]' '[Hh]1' '[Hh]2'; do
@@ -14,4 +14,5 @@ get_title_shtml() {
             return
         fi
     done
+    false  # use fallback
 }
