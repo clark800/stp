fef6eb4840883549c0453777bd6a3edf7d466ce9
Chris Clark <clark800@users.noreply.github.com>
2023-11-27 22:27:40 -0800

    Improve get_title_shtml

--- plugins/get_title_shtml.sh
+++ plugins/get_title_shtml.sh
@@ -2,11 +2,12 @@
 get_title_shtml() {
     _extract() {
         cat "$SOURCE_PATH" | tr -d '\r\n' |
-            sed -n -e "s|</$1>.*|</$1>|" -e "s|.*<$1>\\(.*\\)</$1>.*|\\1|p" \
-                -e 's|^[[:space:]]*||' -e 's|[[:space:]]*$||'
+            sed -n -e "s|</\\($1\\)>.*|</\\1>|" \
+                   -e "s|.*<$1>\\(.*\\)</$1>.*|\\1|p" \
+                   -e 's|^[[:space:]]*||' -e 's|[[:space:]]*$||'
     }
 
-    for tag in title TITLE Title h1 H1 h2 H2; do
+    for tag in '[Tt][Ii][Tt][Ll][Ee]' '[Hh]1' '[Hh]2'; do
         title="$(_extract "$tag")"
         if [ "$title" ]; then
             println "$title"
