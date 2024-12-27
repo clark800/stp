5e43131f56f86ea6a9df4e22394af7f46e63f24b
Chris Clark <clark800@users.noreply.github.com>
2024-12-27 13:16:52 -0800

    Add substitute plugin

---
+++ plugins/substitute.sh
@@ -0,0 +1,28 @@
+# substitute substitutes the output of a function for lines of the form:
+# $(func args)
+
+substitute() {
+    _warn() {
+        println "WARNING: $1" >&2
+    }
+
+    _is_builtin() {
+        /bin/sh -c "[ \"\$(command -v \"$1\")\" == \"$1\" ]"
+    }
+
+    while IFS='' read -r line; do
+        start="${line#\$(}"
+        cmdline="${start%)}"
+        if [ "$start" != "$line" ] && [ "$cmdline" != "$start" ]; then
+            cmd="${cmdline%% *}"
+            if is_function "$cmd" && ! _is_builtin "$cmd"; then
+                $cmdline
+            else
+                _warn "not a function; skipping substitution: $cmdline"
+                println "$line"
+            fi
+        else
+            println "$line"
+        fi
+    done
+}
